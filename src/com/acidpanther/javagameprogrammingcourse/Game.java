421890f7d7ab37450f21a29a08f073983196b449
Aniol Santacreu <asantacreu@compegps.com>
2018-10-18 20:22:32 +0200

    First commit!

---
+++ src/com/acidpanther/javagameprogrammingcourse/Game.java
@@ -0,0 +1,8 @@
+package com.acidpanther.javagameprogrammingcourse;
+
+public class Game {
+	public static int width = 300;
+	public static int height = width / (16 * 9);
+	
+	public static int scale = 3;
+}
