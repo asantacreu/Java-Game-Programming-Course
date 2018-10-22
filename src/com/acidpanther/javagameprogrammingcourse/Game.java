2bad277ca696f0400f2b490c500a4ed0ac8b6e3b
Author Name <aniolsantacreu_1@hotmail.com>
2018-10-23 00:41:06 +0200

    Creates array of pixels

--- src/com/acidpanther/javagameprogrammingcourse/Game.java
+++ src/com/acidpanther/javagameprogrammingcourse/Game.java
@@ -5,8 +5,12 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
+
+import com.acidpanther.javagameprogrammingcourse.graphics.Screen;
 
 
 public class Game extends Canvas implements Runnable {
@@ -21,9 +25,16 @@ public class Game extends Canvas implements Runnable {
 	private JFrame frame;
 	private boolean running = false;
 	
+	private Screen screen;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
+		
+		screen = new Screen(width, height);
 		
 		frame = new JFrame();
 	}
