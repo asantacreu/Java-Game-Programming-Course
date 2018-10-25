c5209ebaaefa26a911c74405f1c520e8705153cd
Aniol Santacreu <asantacreu@compegps.com>
2018-10-25 23:56:31 +0200

    Paints pixels

--- src/com/acidpanther/javagameprogrammingcourse/graphics/Screen.java
+++ src/com/acidpanther/javagameprogrammingcourse/graphics/Screen.java
@@ -2,7 +2,7 @@ package com.acidpanther.javagameprogrammingcourse.graphics;
 
 public class Screen {
 	private int width, height;
-	private int[] pixels;
+	public int[] pixels;
 	
 	public Screen(int width, int height) {
 		this.width = width;
@@ -10,4 +10,11 @@ public class Screen {
 		pixels = new int[width * height];
 	}
 	
+	public void render() {
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
+	}
 }
