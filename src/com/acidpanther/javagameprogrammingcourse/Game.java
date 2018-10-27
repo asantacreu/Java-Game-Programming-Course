7314fda899ebac25c6ec232e1ed989e7874701cb
Author Name <aniolsantacreu_1@hotmail.com>
2018-10-27 12:51:21 +0200

    Adds FPS counter

--- src/com/acidpanther/javagameprogrammingcourse/Game.java
+++ src/com/acidpanther/javagameprogrammingcourse/Game.java
@@ -21,6 +21,8 @@ public class Game extends Canvas implements Runnable {
 	
 	public static int scale = 3;
 	
+	public static String title = "Java GP Course";
+	
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
@@ -56,9 +58,30 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void run() {
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = (1000000000.0 / 60.0);
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
 		while(running) {
-			update();
+			long now = System.nanoTime();
+			delta += ((now - lastTime) / ns);
+			lastTime = now;
+			while(delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
 			render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle(title + " | " + "ups: " + updates + " fps: " + frames);
+				frames = 0;
+				updates = 0;
+			}
 		}
 	}
 
