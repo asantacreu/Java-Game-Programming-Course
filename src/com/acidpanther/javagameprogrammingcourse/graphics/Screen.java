e59b25ed1d5d3179dfafab74f003ae02306d75f0
Author Name <aniolsantacreu_1@hotmail.com>
2018-10-27 21:32:53 +0200

    Creates random tiled map

--- src/com/acidpanther/javagameprogrammingcourse/graphics/Screen.java
+++ src/com/acidpanther/javagameprogrammingcourse/graphics/Screen.java
@@ -1,13 +1,23 @@
 package com.acidpanther.javagameprogrammingcourse.graphics;
 
+import java.util.Random;
+
 public class Screen {
 	private int width, height;
 	public int[] pixels;
+	
+	public int[] tiles = new int[64 * 64];
+	
+	private Random random = new Random();
 	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
+		
+		for (int i = 0; i < tiles.length; i++) {
+			tiles[i] = random.nextInt(0xffffff);
+		}
 	}
 	
 	public void clear() {
@@ -18,8 +28,12 @@ public class Screen {
 	
 	public void render() {
 		for(int y = 0; y < height; y++) {
+			if (y < 0 || y >= height) break;
 			for(int x = 0; x < width; x++) {
-				pixels[x + y * width] = 0xff00ff;
+				if (x < 0 || x >= width) break;
+				
+				int tileIndex = (x >> 4) + (y >> 4) * 64;
+				pixels[x + y * width] = tiles[tileIndex];
 			}
 		}
 	}
