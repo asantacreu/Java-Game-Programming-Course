d7c153a2bd806d5e0bb2b27d1759eea21cc200fe
Aniol Santacreu <aniolsantacreu_1@hotmail.com>
2018-11-12 23:56:23 +0100

    Animates player sprite

--- src/com/acidpanther/javagameprogrammingcourse/entity/mob/Player.java
+++ src/com/acidpanther/javagameprogrammingcourse/entity/mob/Player.java
@@ -9,28 +9,39 @@ public class Player extends Mob{
 	private Keyboard input;
 	private Sprite sprite;
 	private int flipSprite;
+	private int anim = 0;
+	private boolean walking = false;
 	
 	public Player(Keyboard input) {
 		this.input = input;
-		sprite = Sprite.player_up;
+		sprite = Sprite.player_down;
 	}
 	
 	public Player(int x, int y, Keyboard input) {
 		this.x = x;
 		this.y = y;
 		this.input = input;
-		sprite = Sprite.player_up;
+		sprite = Sprite.player_down;
 	}
 	
 	public void update() {
 		int xa = 0;
 		int ya = 0;
+		if(anim< 7500) { 
+			anim++;
+		}
+		else { 
+			anim  = 0;
+		}
 		if(input.up) ya--;
 		if(input.down) ya++;
 		if(input.left) xa--;
 		if(input.right) xa++;
 		
-		if((xa != 0) || (ya != 0)) move(xa, ya);
+		walking = ((xa != 0) || (ya != 0)); 
+		if(walking) {
+			move(xa, ya);
+		}
 	}
 	
 	public void render(Screen screen) {
@@ -45,16 +56,48 @@ public class Player extends Mob{
 		
 		switch(dir) {
 			case 0: 
-				sprite = Sprite.player_down;
+				sprite = Sprite.player_up;
+				if(walking) {
+					if(anim % 20 > 10) {
+						sprite = Sprite.player_up_1;
+					}
+					else {
+						sprite = Sprite.player_up_2;
+					}
+				}
 				break;
 			case 1: 
 				sprite = Sprite.player_right;
+				if(walking) {
+					if(anim % 20 > 10) {
+						sprite = Sprite.player_right_1;
+					}
+					else {
+						sprite = Sprite.player_right_2;
+					}
+				}
 				break;
 			case 2: 
-				sprite = Sprite.player_up;
+				sprite = Sprite.player_down;
+				if(walking) {
+					if(anim % 20 > 10) {
+						sprite = Sprite.player_down_1;
+					}
+					else {
+						sprite = Sprite.player_down_2;
+					}
+				}
 				break;
 			case 3: 
 				sprite = Sprite.player_right;
+				if(walking) {
+					if(anim % 20 > 10) {
+						sprite = Sprite.player_right_1;
+					}
+					else {
+						sprite = Sprite.player_right_2;
+					}
+				}
 				flipSprite = 1;
 				break;
 		}
