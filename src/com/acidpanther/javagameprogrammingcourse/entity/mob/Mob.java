c785f901eb2023b7201450c2fc3cc42128997999
Aniol Santacreu <aniolsantacreu_1@hotmail.com>
2018-11-24 12:34:45 +0100

    Adds corner precision collision detection

--- src/com/acidpanther/javagameprogrammingcourse/entity/mob/Mob.java
+++ src/com/acidpanther/javagameprogrammingcourse/entity/mob/Mob.java
@@ -37,7 +37,15 @@ public abstract class Mob extends Entity {
 	}
 	
 	private boolean collision(int xa, int ya) {
-		return level.getTile((x + xa)/16,  (y + ya)/16).solid();
+		boolean solid = false;
+		
+		for(int c = 0; c < 4; c++) {
+			int xt = ((x + xa) + c % 2 * 14 - 8) / 16;
+			int yt = ((y + ya) + c / 2 * 12 + 3) / 16;
+			if(level.getTile(xt, yt).solid()) solid = true;
+		}
+		
+		return solid;
 	}
 	
 }
