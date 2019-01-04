93b8b070758a452674170279db556f21f76ae36a
Aniol Santacreu <aniolsantacreu_1@hotmail.com>
2019-01-04 23:44:42 +0100

    Pixel-Perfect Collision

--- src/com/acidpanther/javagameprogrammingcourse/entity/projectile/Projectile.java
+++ src/com/acidpanther/javagameprogrammingcourse/entity/projectile/Projectile.java
@@ -37,7 +37,7 @@ public abstract class Projectile extends Entity {
 	}
 	
 	public void update() {
-		if(level.tileCollision(x, y, nx, ny, 7/*sprite.SIZE*/)) {
+		if(level.tileCollision((int)(x + nx), (int)(y + ny), 7, 5, 4)) {
 			level.add(new ParticleSpawner((int) x, (int) y, 44, 50, level));
 			remove();
 		}else {
