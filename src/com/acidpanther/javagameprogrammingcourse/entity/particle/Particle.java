7082393e1ad5108551f62ddfe1f75c290b88b70c
Aniol Santacreu <aniolsantacreu_1@hotmail.com>
2019-01-08 23:56:56 +0100

    Particle Simulation

--- src/com/acidpanther/javagameprogrammingcourse/entity/particle/Particle.java
+++ src/com/acidpanther/javagameprogrammingcourse/entity/particle/Particle.java
@@ -22,10 +22,9 @@ public class Particle extends Entity{
 		this.life = life + (random.nextInt(20) - 10);
 		sprite = Sprite.particle_normal;
 		
-		this.xa = random.nextGaussian() + 1.8;
-		if(xa < 0) xa = 0.1;
+		this.xa = random.nextGaussian();
 		this.ya = random.nextGaussian();
-		this.zz = 2.0;
+		this.zz = random.nextFloat() + 2.0;
 	}
 	
 	public void update() {
@@ -40,18 +39,59 @@ public class Particle extends Entity{
 				xa *= 0.4;
 				ya *= 0.4;
 			}
+			move(xx + xa, (yy + ya) + (zz + za));
 			
-			xx += xa;
-			yy += ya;
-			zz += za;
-		} else {
+		} 
+		else {
 			remove();
 		}
 		
 	}
 	
+	private void move(double x, double y) {
+		if(collision(x, y)) {
+			xa *= -0.5;
+			ya *= -0.5;
+			za *= -0.5;
+		}
+		
+		xx += xa;
+		yy += ya;
+		zz += za;
+	}
+	
+	public boolean collision(double x, double y) {
+		boolean solid = false;
+		
+		for(int c = 0; c < 4; c++) {
+			double xt = (x - c % 2 * 16) / 16;
+			double yt = (y - c / 2 * 16) / 16;
+			
+			int ix;
+			if(c % 2 == 0) {
+				ix = (int) Math.floor(xt);
+			}
+			else {
+				ix = (int) Math.ceil(xt);
+			}
+			
+			int iy;
+			if(c / 2 == 0) {
+				iy = (int) Math.floor(yt);
+			}
+			else {
+				iy = (int) Math.ceil(yt);
+			}
+			if(level.getTile(ix, iy).solid()) {
+				solid = true;
+			}
+		}
+		
+		return solid;
+	}
+
 	public void render(Screen screen) {
-		screen.renderSprite((int) xx - 5, (int) yy - (int) zz, sprite, true);
+		screen.renderSprite((int) xx, (int) yy - (int) zz, sprite, true);
 	}
 	
 }
--- src/com/acidpanther/javagameprogrammingcourse/level/Level.java
+++ src/com/acidpanther/javagameprogrammingcourse/level/Level.java
@@ -90,6 +90,7 @@ public class Level {
 	}
 	
 	public void add(Entity e) {
+		e.init(this);
 		entities.add(e);
 	}
 	
